5e5682340595ffb89da15f85fc4589b1a05bade6
cintiaelisa@gmail.com <cintiaelisa@gmail.com>
2021-05-25 09:15:16 -0300

    Graphql query and mutation

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,40 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const moongoose = require('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const { graphqlHTTP } = require('express-graphql');
 
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+
 const config = require('./config/config.json');
 
-const app  = express();
+const app = express();
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 app.use(cors());
 
-// graphql
-// app.use(
-//     '/graphql',
-//     graphqlHTTP({
-//         schema://,
-//         rootValue: //
-//         graphql: true,
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
 
-//     })
-// )
-
-
-moongoose.connect(
-    `mongodb://localhost:27017/${config.dbname}?retryWrites=true&w=majority`, {
-        useUnifiedTopology: true, useNewUrlParse: true, useCreateIndex: true
+mongoose
+  .connect(
+    `mongodb://localhost:27017/${config.dbname}?retryWrites=true&w=majority`,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
     }
-).then(() => {
-    app.listen(3000, console.log('Connected to Port 3000'))
-}).catch((err) => console.log(err))
+  )
+  .then(() => {
+    app.listen(3000, console.log('Connected to Port 3000.'));
+  })
+  .catch((err) => console.log(err));
