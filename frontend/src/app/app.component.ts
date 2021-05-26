c682a8f7395af8ad69a43545de2d0b4fc78d1097
cintiaelisa@gmail.com <cintiaelisa@gmail.com>
2021-05-26 09:14:37 -0300

    Consuming GraphQL backend from frontend to get quotes.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+
+import gql from 'graphql-tag';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const GET_QUOTES = gql`
+  {
+    quotes {
+      quotes {
+        _id
+        quote
+        author
+      }
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  quotes: Observable<any> = new Observable<any>();
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.quotes = this.apollo
+      .watchQuery({ query: GET_QUOTES })
+      .valueChanges.pipe(
+        map((result: any) => {
+          console.log(result.data.quotes.quotes);
+          return result.data.quotes.quotes;
+        })
+      );
+  }
+
+  create(quote: string , author: string) {
+    console.log(quote, author);
+  }
+  
 }
